d4e2bb8719d77d65fea10c2bf6d1a06d20b8db70
PaolaJAntunez <antunez.duarte20@gmail.com>
2025-03-28 22:06:10 -0600

    Add Facturacion/Properties/AssemblyInfo.cs 1743221169702

---
+++ Facturacion/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sistema de Facturación")]
+[assembly: AssemblyDescription("Sistema completo de facturación con gestión de clientes, productos y reportes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mi Empresa S.A.")]
+[assembly: AssemblyProduct("Facturacion")]
+[assembly: AssemblyCopyright("Copyright © 2023 Mi Empresa S.A.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8a7d3a5f-3f4e-4a2d-9d1e-3e4f5c6d7a8b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
